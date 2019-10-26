19c9c287c53c0091f2c3d390460a7e27cf758d81
Kunal <abc@example.com>
2019-10-26 12:20:25 +0530

    added footer, contact info

--- flasknews/routes.py
+++ flasknews/routes.py
@@ -16,9 +16,9 @@ from flask_login import login_user, current_user, logout_user, login_required
 #Test Cases
 @app.route("/test_var_access", methods = ['GET', 'POST'])
 def test_var_access():
-	value = request.values.get("link")
-	if value:
-		print(value)
+	url = request.values.get("link")
+	if url:
+		print(url)
 	return render_template('test_var_access.html')
 
 
@@ -29,7 +29,12 @@ def home():
 	url = 'https://newsapi.org/v2/top-headlines?country=in&apiKey=afc81dd95663478a88bc39e5cdc851ec'
 	form = QueryForm()
 	title = 'Home'
-	(title,url) = form.on_submit(url,title)
+	(title,url) = form.on_submit(url,title) #if search form is submitted
+	
+	category = request.args.get('category')# if category exits
+	if category:
+		title = category.capitalize()
+		url = 'https://newsapi.org/v2/top-headlines?country=in&category=' + category + '&apiKey=afc81dd95663478a88bc39e5cdc851ec'
 	
 	response = requests.get(url)
 	news = response.json()
@@ -38,6 +43,10 @@ def home():
 @app.route("/about")
 def about():
 	return render_template('about.html', title = 'About Us')
+
+@app.route("/contact")
+def contact():
+	return render_template('contact.html', title = 'Contact')
 
 @app.route("/news")
 def newsDict():
@@ -84,6 +93,7 @@ def logout():
 	return redirect(url_for('home'))
 
 
+# sava_picture() is auxilary funtion for image conversion and savind image in it 'static/profile_pics'
 def save_picture(form_picture):
     random_hex = secrets.token_hex(8)
     _, f_ext = os.path.splitext(form_picture.filename)
@@ -115,76 +125,3 @@ def account():
 		form.email.data = current_user.email
 	image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
 	return render_template('account.html', title='Account',image_file=image_file, form=form)
-
-# News Categories
-
-@app.route("/entertainment", methods = ['GET', 'POST'])
-def entertainment():
-	url = ('https://newsapi.org/v2/top-headlines?country=in&category=entertainment&apiKey=afc81dd95663478a88bc39e5cdc851ec')
-	form = QueryForm()
-	title = 'Entertainment'
-	(title,url) = form.on_submit(url,title)
-	response = requests.get(url)
-	news = response.json()
-	return render_template('home.html',news = news, title = title, form = form)
-
-@app.route("/business", methods = ['GET', 'POST'])
-def business():
-	url = ('https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=afc81dd95663478a88bc39e5cdc851ec')
-	form = QueryForm()
-	title = 'Business'
-	(title,url) = form.on_submit(url,title)
-	response = requests.get(url)
-	news = response.json()
-	return render_template('home.html',news = news, title = title, form = form)
-
-@app.route("/general", methods = ['GET', 'POST'])
-def general():
-	url = ('https://newsapi.org/v2/top-headlines?country=in&category=general&apiKey=afc81dd95663478a88bc39e5cdc851ec')
-	form = QueryForm()
-	title = 'General'
-	(title,url) = form.on_submit(url,title)
-	response = requests.get(url)
-	news = response.json()
-	return render_template('home.html',news = news, title = title, form = form)
-
-@app.route("/health", methods = ['GET', 'POST'])
-def health():
-	url = ('https://newsapi.org/v2/top-headlines?country=in&category=health&apiKey=afc81dd95663478a88bc39e5cdc851ec')
-	form = QueryForm()
-	title = 'Health'
-	(title,url) = form.on_submit(url,title)
-	response = requests.get(url)
-	news = response.json()
-	return render_template('home.html',news = news, title = title, form = form)
-
-@app.route("/science", methods = ['GET', 'POST'])
-def science():
-	url = ('https://newsapi.org/v2/top-headlines?country=in&category=science&apiKey=afc81dd95663478a88bc39e5cdc851ec')
-	form = QueryForm()
-	title = 'Science'
-	(title,url) = form.on_submit(url,title)
-	response = requests.get(url)
-	news = response.json()
-	return render_template('home.html',news = news, title = title, form = form)
-
-@app.route("/sports", methods = ['GET', 'POST'])
-def sports():
-	url = ('https://newsapi.org/v2/top-headlines?country=in&category=sports&apiKey=afc81dd95663478a88bc39e5cdc851ec')
-	form = QueryForm()
-	title = 'Sports'
-	(title,url) = form.on_submit(url,title)
-	response = requests.get(url)
-	news = response.json()
-	return render_template('home.html',news = news, title = title, form = form)
-
-
-@app.route("/technology", methods = ['GET', 'POST'])
-def technology():
-	url = ('https://newsapi.org/v2/top-headlines?country=in&category=technology&apiKey=afc81dd95663478a88bc39e5cdc851ec')
-	form = QueryForm()
-	title = 'Technology'
-	(title,url) = form.on_submit(url,title)
-	response = requests.get(url)
-	news = response.json()
-	return render_template('home.html',news = news, title = title, form = form)
